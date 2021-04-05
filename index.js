27b6fbad0d2fbbc9e55601955a03290616266d80
Krutarth126 <krutarthgupta1206@gmail.com>
2021-04-05 18:32:37 +0530

    first commit

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const data = require("./data.js");
 const ArticleData = require("./dbModule.js");
+const Cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(Cors());
+app.use((req, res, next) => {
+  res.setHeader("Acess-control-allow-origin", "*"),
+    res.setHeader("Acess-control-allow-Header", "*");
+  next();
+});
 
 const connection_url =
   "mongodb+srv://Admin:yashyash@cluster0.hanf4.mongodb.net/test";
@@ -24,7 +31,16 @@ app.get("/readdata", async (req, res) => {
   const result = await ArticleData.find();
   res.status(200).send(result);
 });
-
+app.get("/readdata/:id", async (req, res) => {
+  const id = req.params.id;
+  ArticleData.findById(id, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(data);
+    }
+  });
+});
 app.post("/post", (req, res) => {
   const dbArticle = req.body;
 
